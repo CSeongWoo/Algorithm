8d3ff4baf3d34b277a89440fbc5d5a9818678197
Choi SeongWoo <149999133+CSeongWoo@users.noreply.github.com>
2026-02-06 17:02:42 +0900

    [Silver IV] Title: ATM, Time: 72 ms, Memory: 11848 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		int sum = arr[0];
+		for(int i = 1;i<N; i++) {
+			arr[i] += arr[i - 1];
+			sum += arr[i];
+		}
+		sb.append(sum);
+		System.out.println(sb);
+	}
+}
