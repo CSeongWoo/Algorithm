bd15d81f43ad650cf5eb40561eb2b5427b3b42e4
Choi SeongWoo <149999133+CSeongWoo@users.noreply.github.com>
2026-03-24 17:16:08 +0900

    [Gold V] Title: 트리와 쿼리, Time: 1748 ms, Memory: 99768 KB -BaekjoonHub

---
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N, R, Q;
+	static int[] subTrees;
+	static List<Integer>[] tree;
+	static boolean[] visited;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
+		Q = Integer.parseInt(st.nextToken());
+		int root = R;
+		subTrees = new int[N + 1];
+		tree = new ArrayList[N + 1];
+		visited = new boolean[N + 1];
+		for(int i = 1; i <= N; i++) {
+			tree[i] = new ArrayList<>();
+		}
+		for(int i = 0; i < N - 1; i++) {
+			st = new StringTokenizer(br.readLine());
+			int to = Integer.parseInt(st.nextToken());
+			int from = Integer.parseInt(st.nextToken());
+			tree[to].add(from);
+			tree[from].add(to);
+		}
+		findSubTrees(root);
+		for(int query = 0; query < Q; query++) {
+			st = new StringTokenizer(br.readLine());
+			int vertex = Integer.parseInt(st.nextToken());
+			System.out.println(subTrees[vertex]);
+		}
+	}
+	static int findSubTrees(int index) {
+		visited[index] = true;
+		subTrees[index] = 1;
+		for(int next : tree[index]) {
+			if (visited[next]) continue;
+			subTrees[index] += findSubTrees(next);
+		}
+		return subTrees[index];
+	}
+}
